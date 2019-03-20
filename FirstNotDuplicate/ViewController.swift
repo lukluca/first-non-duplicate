50c6009063caa1d02fd8ca0cc87485d3394c3a03
Luca Tagliabue <homobonus-luca@hotmail.it>
2019-03-20 22:14:19 +0100

    First implementation of UI

--- FirstNotDuplicate/ViewController.swift
+++ FirstNotDuplicate/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var firstNonDuplicateButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.setupTextField()
+        self.setupButton()
+        
     }
 
+    private func setupTextField() {
+        let borderColor =  UIColor(red: 70/255, green: 92/255, blue: 166/255, alpha: 1)
+        inputTextField.layer.borderColor = borderColor.cgColor
+        inputTextField.layer.borderWidth = 2.5
+        
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 1))
+        inputTextField.leftView = paddingView
+        inputTextField.leftViewMode = .always
+    }
+    
+    private func setupButton() {
+        firstNonDuplicateButton.layer.cornerRadius = 10
+    }
 
 }
 
